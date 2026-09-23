69399392d159079ef9432925bc32e3e148b6dd06
dineshlkrishn <48643563+dineshlkrishn@users.noreply.github.com>
2026-09-23 14:25:18 +0000

    Add Gradle wrapper and initial Android project

---
+++ app/src/main/com/Dinesh/voiceassistant/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.yourname.voiceassistant
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val tv = TextView(this)
+        tv.text = "Voice Assistant - Hello World"
+        setContentView(tv)
+    }
+}
